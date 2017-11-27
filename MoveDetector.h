fcccd9501c69acfe7fcebf93e576d8323dc779b2
xintrea <xintrea@gmail.com>
2017-11-27 18:47:50 +0300

    Добавлена структура Marker для представления данных обнаруженного маркера

--- MoveDetector.h
+++ MoveDetector.h
@@ -5,6 +5,26 @@
 #include "opencv2/opencv.hpp"
 #include "CaptureDevice.h"
 
+struct Marker{
+
+    int chunks; // Количество частей
+                // 0 - нет маркера
+                // 1 - одна большая часть (10:3) - для движущегося смазанного и потому "слипшегося" изображения
+                // 2 - две части (1:1 и 2:1) - для четкого изображения
+    QList<QPoinF> verticlesA;
+    QList<QPoinF> verticlesB;
+
+    qfloat massCenterA;
+    qfloat massCenterB;
+
+    qfloat angleA;
+    qfloat angleB;
+
+    QSizeF sizeA;
+    QSizeF sizeB;
+
+};
+
 class MoveDetector
 {
 public:
